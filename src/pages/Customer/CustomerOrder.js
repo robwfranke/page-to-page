89dcbc1ee1e139e17a8000fc35c0c7e465aa8412
=Rob Franke <robwfranke@gmail.com>
2021-06-19 14:51:32 +0200

    op weg om item toe te voegen

--- src/pages/Customer/CustomerOrder.js
+++ src/pages/Customer/CustomerOrder.js
@@ -9,9 +9,6 @@ import jwt_decode from "jwt-decode";
 function CustomerOrder() {
 
 
-
-
-
     const {register, handleSubmit, formState: {errors}} = useForm();
     const history = useHistory();
 
@@ -20,8 +17,7 @@ function CustomerOrder() {
     const orderIndividual = location.state.order;
 
 
-    console.log("orderIndividual: ",orderIndividual)
-
+    console.log("orderIndividual: ", orderIndividual)
 
 
     const loadOrder = localStorage.getItem('loadOrder')
@@ -29,26 +25,39 @@ function CustomerOrder() {
     const token = localStorage.getItem('token');
     const decoded = jwt_decode(token);
     const userNameInCustomerOrder = decoded.sub;
-
-
-
-
     const [changeStatus, setChangeStatus] = useState(false);
+    const[addItemStatus, setAddItemStatus]=useState(false);
+    const [disableButton, setDisableButton] = useState(false);
 
-    function StartChange() {
+
+    function StartChangeStatus() {
 
         setChangeStatus(true);
+        setDisableButton(true);
         console.log("setChangeOrder true")
     }
 
 
-    function cancel1() {
-        // setLoadOrderState(true);
-        // setAddOrderStatus(false)
+    function cancelCustomerOrder() {
+
         history.push("/customer")
 
     }
 
+    function cancelChangeStatusOrder() {
+        setChangeStatus(false);
+    }
+
+    function canceladdItem() {
+        setAddItemStatus(false);
+    }
+
+
+    function addItem() {
+        setChangeStatus(false);
+        setAddItemStatus(true);
+
+    }
 
 
     async function onSubmit(data) {
@@ -57,14 +66,21 @@ function CustomerOrder() {
         if (changeStatus === true) {
             // localStorage.setItem('loadOrder', true);
 
-            console.log("data in onSubmit", data)
+            console.log("data in onSubmit van changeStatus", data)
             putStatus(data);
             setChangeStatus(false)
             localStorage.setItem('loadOrder', true);
             history.push("/customer")
+        }
 
+
+        if (addItemStatus===true){
+
+            console.log("data in onSubmit van addItem", data)
 
         }
+
+
 
 
     }
@@ -83,9 +99,8 @@ function CustomerOrder() {
         try {
             console.log("PutStatus")
             console.log("dataPut, ordername: ", dataPut.ordername)
-            console.log("dataPut, status: ",dataPut.status)
-            console.log("dataPut, userNameInCustomerOrder: ",userNameInCustomerOrder)
-
+            console.log("dataPut, status: ", dataPut.status)
+            console.log("dataPut, userNameInCustomerOrder: ", userNameInCustomerOrder)
 
 
             const response = await axios.put(`http://localhost:8080/orders/update/${orderIndividual.ordername}`, dataPut, {
@@ -94,7 +109,6 @@ function CustomerOrder() {
                     Authorization: `Bearer ${token}`, /*BACK TICK!!!!!*/
                 }
             })
-
 
 
         } catch (e) {
@@ -111,70 +125,168 @@ function CustomerOrder() {
 
         <section>
             <h1>CustomerOrder pagina</h1>
-            <h2>{orderIndividual.ordername}</h2>
-            <h3>status: {orderIndividual.status}</h3>
+
 
 
             <fieldset className={styles["listItem-buttons"]}>
 
-            <button
-                type="text"
-                onClick={StartChange}
-            >
-                Wijzig status
-            </button>
+                <button
+                    type="text"
+                    onClick={StartChangeStatus}
+                    className={styles["submit-button"]}
+                >
+                    Wijzig status
+                </button>
 
 
                 <button
-                    onClick={cancel1}
+                    type="text"
+                    onClick={addItem}
+                    className={styles["submit-button"]}
+                >
+                    Voeg item toe!
+                </button>
+
+
+                <button
+                    onClick={cancelCustomerOrder}
                     className={styles["submit-button"]}
                 >
                     Cancel
                 </button>
 
 
+                <>
 
 
-            <>
+                    {changeStatus &&
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <fieldset className={styles["registration-container"]}>
 
 
-                {changeStatus &&
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <fieldset className={styles["registration-container"]}>
+                            <p>Selecteer status</p>
+                            <select
+                                {...register("status",)}
+                            >
+                                <option value="open">open</option>
+                                <option value="pending">pending</option>
+                                <option value="finished">finished</option>
+                            </select>
+
+
+                            <button
+                                type="submit"
+                            >
+                                Wijzig!
+                            </button>
+
+
+                            <button
+                                onClick={cancelChangeStatusOrder}
+                                type="text"
+                            >
+                                Cancel
+                            </button>
+
+
+                        </fieldset>
+                    </form>
+                    }
 
 
 
 
-                        <p>Status</p>
-                        <select
-                            {...register("status",)}
-                        >
-                            <option value="open">open</option>
-                            <option value="pending">pending</option>
-                            <option value="finished">finished</option>
-                        </select>
+                    {addItemStatus &&
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <fieldset className={styles["registration-container"]}>
 
 
-                        <button
-                            type="submit"
-                        >
-                            Wijzig!
-                        </button>
+                            <p>Voeg item toe</p>
+                            <label htmlFor="itemname-field">
+                                Username:
+                                <input
+                                    type="text"
+
+                                    placeholder="vb. dwg 2021-001"
+                                    {...register("itemname", {required: true})}
+                                />
+                                {errors.itemname && (
+                                    <span className={styles["alert"]}>Vul uw itemname in</span>
+
+                                )}
+                            </label>
 
 
-                    </fieldset>
-                </form>
-                }
+
+                            <label htmlFor="quantity-field">
+                                Aantal:
+                                <input
+                                    type="integer"
+
+                                    placeholder="vb. dwg 2021-001"
+                                    {...register("quantity", {
+                                        required: true,
+                                    min:1
+                                    })}
+                                />
+                                {errors.quantity && (
+                                    <span className={styles["alert"]}>aantal groter dan nul</span>
+
+                                )}
+                            </label>
 
 
-            </>
+
+
+
+
+
+
+
+
+
+
+                            <button
+                                type="submit"
+                            >
+                                Voeg toe!
+                            </button>
+
+
+                            <button
+                                onClick={canceladdItem}
+                                type="text"
+                            >
+                                Cancel
+                            </button>
+
+
+                        </fieldset>
+                    </form>
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                </>
             </fieldset>
 
+            <h2>{orderIndividual.ordername}</h2>
+            <h3>status: {orderIndividual.status}</h3>
 
             <ul>
                 {orderIndividual.items.map((item) => {
                     return <li key={item.id}>
-
 
 
                         <NavLink
@@ -194,19 +306,12 @@ function CustomerOrder() {
                         </NavLink>
 
 
-
-
                         {/*<div>Naam: {item.itemname} </div>*/}
                         <div>Quantity: {item.quantity} </div>
                         <div>jobs: {item.jobsFromItem.length} </div>
                     </li>
                 })}
             </ul>
-
-
-
-
-
 
 
         </section>
